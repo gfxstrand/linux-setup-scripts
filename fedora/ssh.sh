37bf639345f807f7267fed15a2a14bc49d57c428
Jason Ekstrand <jason@jlekstrand.net>
2013-12-24 22:05:13 -0600

    Add an ssh script.  This should serve as a reminder that it's not available by default

---
+++ fedora/ssh.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Fedora setup script for Fedora >= 19
+#
+# This set of scripts sets up a new fedora install to my personal tastes
+# including default software installs, shell configuration, etc.
+#
+# This particular script sets up ssh.
+#
+# To be run as root.
+
+# Install zsh, git, and vim.  The essential tools for life.
+yum -y install openssh-server
+systemctl start sshd.service
+systemctl enable sshd.service
+
