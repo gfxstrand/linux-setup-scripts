dd16e051068dd3a907bf498f97362e393e450239
Jason Ekstrand <jason.ekstrand@intel.com>
2015-09-26 14:08:58 -0700

    Various updates for building mesa

--- fedora/mesa-devel.sh
+++ fedora/mesa-devel.sh
@@ -11,15 +11,19 @@
 # Install devel stuff as well
 `dirname $0`/devel.sh
 
+# Install useful tools
+$YUM -y install apitrace
+
 # Install mesa dependancies
 yum-builddep -y mesa
-$YUM -y install nettle-devel libdrm-devel systemd-devel \
-        libpciaccess-devel install llvm-static
+$YUM -y install python-mako expat-devel bison byacc flex nettle-devel \
+                libdrm-devel systemd-devel libpciaccess-devel llvm-static \
+                libwayland-client-devel libwayland-server-devel
 
 # Install piglit mesa dependancies
 yum-builddep -y piglit
-$YUM -y install waffle-devel python-mako expat-devel bison byacc flex
-$YUM -y install ninja-build mesa-libEGL-devel mesa-libgbm-devel
+$YUM -y install waffle-devel ninja-build mesa-libEGL-devel \
+                mesa-libgbm-devel libcaca-devel
 
 # Enable rendernodes and disable the command parser on boot
 sed -i -e 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 drm.rnodes=1 i915.enable_cmd_parser=0"/' /etc/default/grub
