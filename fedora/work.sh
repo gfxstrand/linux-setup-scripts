8b68bfd5a06ccb08ab474397cb0f461e94b91998
Jason Ekstrand <jason.ekstrand@intel.com>
2017-01-19 20:25:02 -0800

    Add VPN script

---
+++ fedora/work.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Fedora setup script for Fedora >= 19
+#
+# This set of scripts sets up a new fedora install to my personal tastes
+# including default software installs, shell configuration, etc.
+#
+# This particular script sets up the packages I need to connect to VPN etc.
+# for my job.
+#
+# To be run as root.
+
+# Install dependencies for setting up VPN
+$YUM -y install python-PKI NetworkManager-openconnect-gnome
