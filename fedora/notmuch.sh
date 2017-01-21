6a7d89eba0a3b26868c1495d78ec25e718c64225
Jason Ekstrand <jason.ekstrand@intel.com>
2017-01-21 12:55:57 -0800

    Add a config for notmuch

---
+++ fedora/notmuch.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Fedora setup script for Fedora >= 19
+#
+# This set of scripts sets up a new fedora install to my personal tastes
+# including default software installs, shell configuration, etc.
+#
+# This particular script sets up the machine for e-mail via notmuch and vim
+#
+# To be run as root.
+
+$YUM -y install isync msmtp notmuch notmuch-vim ruby-notmuch elinks
